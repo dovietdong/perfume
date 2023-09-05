78be9042305019a4a513269d25e02abfbf976e89
dovietdong <dovietdong2507@gmail.com>
2023-09-05 21:00:01 +0700

    thanh toan nvpay

--- pages/handle/config_vnpay.php
+++ pages/handle/config_vnpay.php
@@ -9,7 +9,9 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
 $vnp_TmnCode = "MCG9RE1Q"; //Mã định danh merchant kết nối (Terminal Id)
 $vnp_HashSecret = "BPZPZWGMUSPMKQAUFMWOYVLKTVYBBWLX"; //Secret key
 $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-$vnp_Returnurl = "http://localhost:8080/guhastorephp/index.php?page=thankiu";
+//$vnp_Returnurl = "http://localhost:8080/guhastorephp/index.php?page=thankiu";
+$vnp_Returnurl = "https://perfume-5ac10d47075a.herokuapp.com//guhastorephp/index.php?page=thankiu";
+
 $vnp_apiUrl = "http://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
 $apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 //Config input format
